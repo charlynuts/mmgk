821d066a9fd8a909dcfd714b466ceca25c0f40d0
LukasDotEU <77338524+LukasDotEU@users.noreply.github.com>
2021-02-23 18:19:55 +0100

    Add files via upload

---
+++ Hauptseite.js
@@ -0,0 +1,34 @@
+//Code teils aus folgender Quelle: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+var mybutton;
+function init()
+{
+  mybutton = document.getElementById("myBtn");
+
+  var Nachname = sessionStorage.getItem("name");
+  if (Nachname==null)
+  {
+    document.getElementById('Ansprache').parentNode.removeChild(document.getElementById('Ansprache'));
+  }
+  else
+  {
+    document.getElementById('AnspracheText').innerHTML += Nachname;
+  }
+}
+
+// When the user scrolls down 250px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+window.addEventListener('DOMContentLoaded',init);
