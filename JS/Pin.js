fa6f9ad65d69217fbd0b6828f239ee471e771634
LukasDotEU <77338524+LukasDotEU@users.noreply.github.com>
2021-02-24 22:25:32 +0100

    Add files via upload

---
+++ JS/Pin.js
@@ -0,0 +1,30 @@
+function init2 ()
+{
+  var Pin = document.getElementById('Pin');
+  document.getElementById("Formular").addEventListener("input", ueberpruefe);
+}
+
+function Quersumme (Zahl)
+{
+  var quer = 0;
+  var zwischen = 0;
+  while (Zahl !=0)
+  {
+    zwischen=Zahl%10
+    quer+=zwischen
+    Zahl=(Zahl-zwischen)/10
+  }
+  return quer;
+}
+
+function ueberpruefe() {
+  if ( Pin.valueAsNumber % 3 == 0 && Pin.valueAsNumber % 5 == 0 && Pin.valueAsNumber % 13 == 0 && Quersumme(Pin.valueAsNumber)==27)
+  {
+    document.getElementById('Antwort').value = "Richtig";
+  }
+  else {
+    document.getElementById('Antwort').value = "Falsch";
+  }
+}
+
+window.addEventListener('DOMContentLoaded',init2);
